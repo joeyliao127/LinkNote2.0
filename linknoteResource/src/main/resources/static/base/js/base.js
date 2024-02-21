2ce933cd019c5a1c969ae11494f270f248a51be7
JoeyLiao <joey01271998@gmail.com>
2024-02-21 14:44:46 +0800

    Resource: 前端JS撰寫撰寫

--- linknoteResource/src/main/resources/static/base/js/base.js
+++ linknoteResource/src/main/resources/static/base/js/base.js
@@ -1,41 +1,64 @@
-const token = localStorage.getItem("token");
-function baseInit() {
-  if (token) {
-    verifyUserToken(token);
-  } else {
-    window.location.href = "/";
+class AuthorizationHandler {
+  constructor() {
+    this.token = localStorage.getItem("token");
+    if (token) {
+      this.verifyUserToken(token);
+    } else {
+      window.location.href = "/";
+    }
+  }
+
+  static fetchPath = "http://localhost";
+
+  verifyUserToken = async function (token) {
+    const response = await fetch("http://localhost:8080/api/auth/user/token", {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const verifyResult = await response.json();
+    if (!verifyResult) {
+      localStorage.removeItem("token");
+      location.href = "/";
+    }
+  };
+}
+
+class fetchDataHandler {
+  constructor() {}
+
+  static async fetchData(path, method, body) {
+    const response = await fetch(path, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      method: method,
+      body: JSON.stringify(body),
+    });
+    if (response.status === 401) {
+      window.location.href = "/notebooks";
+    }
+    return response.json();
   }
 }
 
-async function verifyUserToken(token) {
-  const response = await fetch("/api/user/auth", {
-    headers: {
-      Authorization: "Bearer " + token,
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-  });
-  const verifyResult = await response.json();
-  if (!verifyResult) {
-    localStorage.removeItem("token");
-    location.href = "/";
-  }
-}
-
-async function fetchData(path, method, body) {
-  const response = await fetch(path, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-    method: method,
-    body: JSON.stringify(body),
-  });
-  if (response.status === 401) {
-    window.location.href = "/notebooks";
-  }
-  return response.json();
-}
+// async function verifyUserToken(token) {
+//   const response = await fetch("http://localhost:8080/api/auth/user/token", {
+//     headers: {
+//       Authorization: "Bearer " + token,
+//       "Content-Type": "application/json",
+//     },
+//     method: "POST",
+//   });
+//   const verifyResult = await response.json();
+//   if (!verifyResult) {
+//     localStorage.removeItem("token");
+//     location.href = "/";
+//   }
+// }
 
 function verifyEmailRegx(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
--- linknoteResource/src/main/resources/static/index/js/index.js
+++ linknoteResource/src/main/resources/static/index/js/index.js
@@ -62,7 +62,7 @@ function register() {
 }
 
 async function fetchRegisterEndpoint(username, email, password) {
-  const endpoint = "/api/user/register";
+  const endpoint = ":8080/api/user/register";
   const requestBody = { username, email, password };
   const request = {
     method: "POST",
@@ -102,7 +102,7 @@ function signInBtnListener() {
   signinBtn.addEventListener("click", async () => {
     const email = document.querySelector("#signin-email").value;
     const password = document.querySelector("#signin-password").value;
-    const endpoint = "/api/user/auth";
+    const endpoint = "localhost:8080/api/auth/user/signin";
     const reqBody = { email: email, password: password };
     console.log(`email: ${email}, ps:${password}`);
     const checkInput = validateEmailFormatAndPasswordNotNull(email, password);
@@ -134,7 +134,7 @@ function signInBtnListener() {
 }
 
 async function verifyUserByToken(token) {
-  const response = await fetch("/api/user/auth", {
+  const response = await fetch(":8080/api/auth/user/token", {
     headers: {
       Authorization: "Bearer " + token,
       "Content-Type": "application/json",
